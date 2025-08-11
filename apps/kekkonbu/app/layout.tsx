97db2257b4ff6ff5174faf1ca1c6a525a9de1295
vdnih <59527453+vdnih@users.noreply.github.com>
2025-08-11 18:24:58 +0900

    feat: add Google Analytics to kekkonbu

--- apps/kekkonbu/app/layout.tsx
+++ apps/kekkonbu/app/layout.tsx
@@ -5,6 +5,7 @@ import Footer from '@/components/Footer';
 import Nav from '@/components/Nav';
 import './globals.css';
 import styles from './layout.module.css';
+import Script from 'next/script';
 
 export const metadata = {
   metadataBase: new URL(process.env.BASE_URL || 'http://localhost:3000'),
@@ -12,7 +13,8 @@ export const metadata = {
   description: '婚約から海外挙式までの実体験とChatGPT活用法で、理想の結婚準備を効率化するブログ。',
   openGraph: {
     title: '結婚部',
-    description: '婚約から海外挙式までの実体験とChatGPT活用法で、理想の結婚準備を効率化するブログ。',
+    description:
+      '婚約から海外挙式までの実体験とChatGPT活用法で、理想の結婚準備を効率化するブログ。',
     images: '/ogp.png',
   },
   alternates: {
@@ -29,6 +31,15 @@ export default async function RootLayout({ children }: Props) {
   return (
     <html lang="ja">
       <head>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-BLY3P4C351" />
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-BLY3P4C351');
+          `}
+        </Script>
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3598027223624482"
